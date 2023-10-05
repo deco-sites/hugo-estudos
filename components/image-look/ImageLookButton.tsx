6728306d9619586d95a7eb77e8051f820eb9494e
HugoLc <hugolindoso.c@gmail.com>
2023-10-05 18:32:53 -0300

    fix: ajusta card de informações

--- components/image-look/ImageLookButton.tsx
+++ components/image-look/ImageLookButton.tsx
@@ -1,15 +1,49 @@
-import { memo, useState } from "preact/compat";
-import InfoCard from "deco-sites/hugo-estudos/components/image-look/InfoCard.tsx";
+import {
+  lazy,
+  memo,
+  Suspense,
+  useEffect,
+  useRef,
+  useState,
+} from "preact/compat";
 import { Buttons } from "deco-sites/hugo-estudos/components/image-look/Types.ts";
+import Spinner from "deco-sites/hugo-estudos/components/ui/Spinner.tsx";
+
+const InfoCard = lazy(() =>
+  import("deco-sites/hugo-estudos/components/image-look/InfoCard.tsx")
+);
 
 const ImageLookButton = (
   { positionX, positionY, product, card }: Buttons,
 ) => {
   const [openInfoCard, setOpenInforCard] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      buttonRef.current && !buttonRef.current.contains(event.target as Node)
+    ) {
+      setOpenInforCard(false);
+    }
+  };
+
+  useEffect(() => {
+    // Adicione um ouvinte de evento de clique global
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Remova o ouvinte quando o componente for desmontado
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   if (!product) return null;
   return (
     <button
-      className={`absolute bg-red-200 translate-x-[-50%] translate-y-[-50%] w-5 h-5 rounded-full w-`}
+      class={`absolute bg-secondary border-2 border-white font-bold flex justify-center leading-5 items-center text-white z-10 translate-x-[-50%] translate-y-[-50%] w-5 h-5 rounded-full ${
+        openInfoCard ? "z-50" : "z-10"
+      }`}
+      ref={buttonRef}
       style={{
         top: `${positionX}%`,
         left: `${positionY}%`,
@@ -17,7 +51,11 @@ const ImageLookButton = (
       onClick={() => setOpenInforCard((prev) => !prev)}
     >
       +
-      {openInfoCard && <InfoCard product={product}  card={card}/>}
+      {openInfoCard && (
+        <Suspense fallback={<Spinner />}>
+          <InfoCard product={product} card={card} />
+        </Suspense>
+      )}
     </button>
   );
 };
--- components/image-look/InfoCard.tsx
+++ components/image-look/InfoCard.tsx
@@ -1,5 +1,6 @@
 import { Product } from "apps/commerce/types.ts";
 import { Card } from "deco-sites/hugo-estudos/components/image-look/Types.ts";
+import { useEffect, useState } from "preact/compat";
 
 interface Props {
   product: Product;
@@ -9,19 +10,28 @@ interface Props {
 const InfoCard = (
   { product, card: { cardPositionX, cardPositionY } }: Props,
 ) => {
+  const [isVisible, setisVisible] = useState(false);
+  useEffect(() => {
+    setTimeout(
+      () => setisVisible(true),
+      50,
+    );
+  }, []);
+
   if (!product) return null;
   return (
     <div
-      className="absolute bg-white shadow-2xl w-max min-w-[200px] max-w-full border-black  border top-[50%] left-[50%] p-2"
-      // style={{ top: `${cardPositionY}%`, left: `${cardPositionX}` }}
+      class={`absolute bg-white text-black shadow-2xl w-max min-w-[200px] max-w-full border-black  border top-[50%] left-[50%] p-2 transition-all duration-300 ease-in-out ${
+        isVisible ? "opacity-100" : "opacity-0"
+      }`}
       style={{ transform: `translate(${cardPositionY}% , ${cardPositionX}%)` }}
     >
       <a href={product.url} target="_blank">
-        <p className="whitespace-nowrap font-bold overflow-hidden text-ellipsis max-w-full">
+        <p className="font-bold overflow-hidden text-ellipsis max-w-full mb-1.5">
           {product.name}
         </p>
-        <p>{product.offers?.lowPrice}</p>
-        <button className="cursor-pointer pointer-events-none">
+        <p class="font-normal mb-1.5">{product.offers?.lowPrice}</p>
+        <button className="cursor-pointer font-normal pointer-events-none p-1 bg-secondary rounded-md text-white">
           Ver mais
         </button>
       </a>
