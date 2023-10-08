908d21ea570774db9ed657f01ada59f1eea04d54
Hugo <hugo@m3ecommerce.com>
2023-10-08 16:44:02 -0300

    fix: altera a tag img para picture da deco

--- components/image-look/ImageLook.tsx
+++ components/image-look/ImageLook.tsx
@@ -1,5 +1,6 @@
 import { Props } from "deco-sites/hugo-estudos/components/image-look/Types.ts";
 import ImageLookButton from "deco-sites/hugo-estudos/islands/ImageLookButton.tsx";
+import { Picture, Source } from "apps/website/components/Picture.tsx";
 
 function ImageLook({ images, title }: Props) {
   return (
@@ -10,13 +11,33 @@ function ImageLook({ images, title }: Props) {
             {title}
           </h2>
           <div className="relative w-auto inline-block">
-            <img
+            {
+              /* <img
               src={image.src}
               alt={image.alt}
               width={500}
               height={500}
               loading="eager"
-            />
+            /> */
+            }
+            <Picture
+              preload
+              class="col-start-1 col-span-1 row-start-1 row-span-1"
+            >
+              <Source
+                src={image.src}
+                width={300}
+                height={300}
+                media="(max-width: 767px)"
+              />
+              <Source
+                src={image.src}
+                width={500}
+                height={500}
+                media="(min-width: 767px)"
+              />
+              <img src={image.src} alt={image.alt ?? title} />
+            </Picture>
             {image.buttons.map((button, index) => {
               if (!button.product) return;
               return (
