4e6a794fe6bd650531f64415df2b77708878eca8
HugoLc <hugolindoso.c@gmail.com>
2023-10-16 18:53:38 -0300

    fix: remove socket

--- main.ts
+++ main.ts
@@ -11,44 +11,44 @@ import decoManifest from "./manifest.gen.ts";
 
 // SOCKET
 
-import { serve } from "https://deno.land/std@0.150.0/http/server.ts";
-import { Server } from "https://deno.land/x/socket_io@0.1.1/mod.ts";
+// import { serve } from "https://deno.land/std@0.150.0/http/server.ts";
+// import { Server } from "https://deno.land/x/socket_io@0.1.1/mod.ts";
 
-const io = new Server({
-  cors: {
-    origin: "http://localhost:8000",
-    methods: ["GET", "POST"],
-  },
-});
+// const io = new Server({
+//   cors: {
+//     origin: "http://localhost:8000",
+//     methods: ["GET", "POST"],
+//   },
+// });
 
-io.on("connection", (socket) => {
-  socket.emit("me", socket.id);
+// io.on("connection", (socket) => {
+//   socket.emit("me", socket.id);
 
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("callEnded");
-  });
+//   socket.on("disconnect", () => {
+//     socket.broadcast.emit("callEnded");
+//   });
 
-  socket.on("callUser", (data) => {
-    console.log("main.ts -> data", data);
+//   socket.on("callUser", (data) => {
+//     console.log("main.ts -> data", data);
 
-    io.to(data.userToCall).emit("callUser", {
-      signal: data.signalData,
-      from: data.from,
-      name: data.name,
-    });
-  });
+//     io.to(data.userToCall).emit("callUser", {
+//       signal: data.signalData,
+//       from: data.from,
+//       name: data.name,
+//     });
+//   });
 
-  socket.on("answerCall", (data) => {
-    io.to(data.to).emit("callAccepted", data.signal);
-  });
-  socket.on("message", (data) => {
-    console.log(`Mensagem recebida: ${data}`);
-  });
-});
+//   socket.on("answerCall", (data) => {
+//     io.to(data.to).emit("callAccepted", data.signal);
+//   });
+//   socket.on("message", (data) => {
+//     console.log(`Mensagem recebida: ${data}`);
+//   });
+// });
 
-serve(io.handler(), {
-  port: 5000,
-});
+// serve(io.handler(), {
+//   port: 5000,
+// });
 
 // FIM SOCKET
 
